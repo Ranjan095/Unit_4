a7ad80b947e988aa3a9e2c2d338d8029f9ba32ac
Ranjan Yadav <iranjan095@gmail.com>
2023-01-11 02:14:57 +0530

    I have update the action

--- useReducer/usereducer/src/Conponent/UseReduce.jsx
+++ useReducer/usereducer/src/Conponent/UseReduce.jsx
@@ -2,15 +2,38 @@ import React from "react"
 import { useState } from "react"
 
 export default function UseReduce(){
-let [count,setCount]=useState(0)
+// let [count,setCount]=useState(0)
 
-    return (
-        <div>
-            <h1>Data testing....</h1>
+//     return (
+//         <div>
+//             <h1>Data testing....</h1>
 
-            <h1>{count}</h1>
-            <button  onClick={()=>setCount(preCount=>preCount+1)}>add</button>
-            <button disabled={count==0} onClick={()=>setCount(preCount=>preCount-1)}>reduce</button>
-        </div>
-    )
+//             <h1>{count}</h1>
+//             <button  onClick={()=>setCount(preCount=>preCount+1)}>add</button>
+//             <button disabled={count==0} onClick={()=>setCount(preCount=>preCount-1)}>reduce</button>
+//         </div>
+//     )
+
+
+function calBack(state,action){
+
+ switch(action.type){
+case 'add' :{
+    return state + 1
+} 
+case 'sub':{
+   return state -1;
+}
+default:{
+    return 'change the action'
+}
+}
+
+}
+let res=calBack(10,{type:'add'})
+console.log(res)
+
+
+
+
 }
